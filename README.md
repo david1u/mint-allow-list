2940d584d975a8f86b4e0e06de5771434d48cf79
Jerry <jf4580@nyu.edu>
2023-07-06 12:03:37 -0400

    Rest of API functions. Added APIkey to each function cause my dad said we need to input that.

--- README.md
+++ README.md
@@ -10,25 +10,23 @@ different interaction types:
 - [Select menus](https://discord.com/developers/docs/interactions/message-components#select-menus)
 - [Modals (with text inputs)](https://discord.com/developers/docs/interactions/message-components#text-inputs)
 
-/poll allows you to create a poll and keep count of votes from users. To create the poll, enter your question in the top text box and enter the different answers to the poll, each on a different line. For example, in the top box if your question is "What is your favorite color?", enter a few different colors in the bottom box with each color on a new line. For example:
-red
-green
-blue
-would create a new poll with three different color options. A user can only vote once in a poll. 
+SPEARMINT API ALLOW LIST
 
 # Try it out
 
-npm run build
-npm run server
-ngrok http 3000
+npm run build npm run server ngrok http 3000
+
 ```
 
 In your Discord server:
 
 ```
+
 /test-flight install <https-url-from-ngrok>/poll-action
+
 ```
 
 ![Slash commands](docs/poll-action-commands.png)
 
 ![Screenshot](docs/poll-action.png)
+```
--- src/actions/spearmint-api.ts
+++ src/actions/spearmint-api.ts
@@ -1,81 +1,151 @@
-import { getFetch, handleFetchResponse } from '@collabland/common';
+import {getFetch, handleFetchResponse} from '@collabland/common';
 
 type responseAPI = {
-    data: Array<{
-        //id, name ,creator, community id, entries[]
-        name: string;
-        type: string;
-        defaultValue: string | boolean;
-    }>;
+  data: Array<{
+    //id, name ,creator, community id, entries[]
+    name: string;
+    type: string;
+    defaultValue: string | boolean;
+  }>;
+};
+type GetEntryStatusAPI = {
+  data: {
+    status:
+      | 'submitted'
+      | 'selected'
+      | 'waitlisted'
+      | 'not_selected'
+      | 'disqualified';
+  };
+};
+type GetEntryAPI = {
+  data: {
+    address: string;
+    status:
+      | 'submitted'
+      | 'selected'
+      | 'waitlisted'
+      | 'not_selected'
+      | 'disqualified';
+    tentativeStatus:
+      | 'selected'
+      | 'waitlisted'
+      | 'not_selected'
+      | 'disqualified';
+    attestationData: Record<string, string | boolean> | null;
+  };
+};
+type GetEntryResponseAPI = {
+  data: {
+    address: string;
+    status:
+      | 'submitted'
+      | 'selected'
+      | 'waitlisted'
+      | 'not_selected'
+      | 'disqualified';
+    tentativeStatus:
+      | 'selected'
+      | 'waitlisted'
+      | 'not_selected'
+      | 'disqualified';
+    attestationData: Record<string, string | boolean> | null;
+  };
 };
 
-type entryResponseAPI = {
-    data: {
-        address: string;
-        status: "submitted" | "selected" | "waitlisted" | "not_selected" | "disqualified";
-        tentativeStatus: "selected" | "waitlisted" | "not_selected" | "disqualified";
-        attestationData: Record<string, string | boolean> | null;
-    };
-}
-
+type GetProofAPIResponse = {
+  data: {
+    proof: string[];
+    attestationData: Record<string, string | boolean> | null;
+  };
+};
 
 export class ListAPI {
-    private fetch = getFetch({
-        headers: { 'api-key': 'spsk_9U84OvT1ReIc8r0XP3nPZQsUEwYVnDtCMdtWajfI' }, //might need user to submit their own API key as well
-    });
+  private fetch = getFetch({
+    headers: {'api-key': 'spsk_9U84OvT1ReIc8r0XP3nPZQsUEwYVnDtCMdtWajfI'}, //might need user to submit their own API key as well
+  });
 
-    async createAttestationSchema(projectID: string) {
-        projectID = '3fd819d8-8bd5-4d5b-a3b4-ae4820b58bf4';
-        const response = await this.fetch(
-            `https://api.spearmint.xyz/projects/${projectID}/attestationSchema`
-            //'https://api.spearmint.xyz/projects/3fd819d8-8bd5-4d5b-a3b4-ae4820b58bf4/attestationSchema'
-            ,
-            {
-                method: 'PUT',
-                headers: {
-                    accept: 'application/json',
-                    Authorization: 'spsk_PiosaAbiHXn5I1paVlREGP5WfQZ5IleAzwBkSdtL',
-                    'content-type': 'application/json'
-                }
-            },
-        );
-        const data = await handleFetchResponse<responseAPI>(response);
-        return data;
-    }
+  async createAttestationSchema(projectID: string) {
+    projectID = '3fd819d8-8bd5-4d5b-a3b4-ae4820b58bf4';
+    const response = await this.fetch(
+      `https://api.spearmint.xyz/projects/${projectID}/attestationSchema`,
+      //'https://api.spearmint.xyz/projects/3fd819d8-8bd5-4d5b-a3b4-ae4820b58bf4/attestationSchema'
+      {
+        method: 'PUT',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'spsk_PiosaAbiHXn5I1paVlREGP5WfQZ5IleAzwBkSdtL',
+          'content-type': 'application/json',
+        },
+      },
+    );
+    const data = await handleFetchResponse<responseAPI>(response);
+    return data;
+  }
 
-    async createOrUpdateEntry(projectID: string, address: string) {
-        projectID = '3fd819d8-8bd5-4d5b-a3b4-ae4820b58bf4';
-        const response = await this.fetch(
-            `https://api.spearmint.xyz/projects/${projectID}/attestationSchema`
-            //'https://api.spearmint.xyz/projects/3fd819d8-8bd5-4d5b-a3b4-ae4820b58bf4/attestationSchema'
-            ,
-            {
-                method: 'PUT',
-                headers: {
-                    accept: 'application/json',
-                    Authorization: 'spsk_PiosaAbiHXn5I1paVlREGP5WfQZ5IleAzwBkSdtL',
-                    'content-type': 'application/json'
-                },
-                body: JSON.stringify({ tentativeStatus: 'selected', shouldFinalize: false })
-            },
-        );
-        const data = await handleFetchResponse<entryResponseAPI>(response);
-        return data;
-    }
+  async createOrUpdateEntry(
+    projectID: string,
+    apiKey: string,
+    address: string,
+  ) {
+    projectID = '3fd819d8-8bd5-4d5b-a3b4-ae4820b58bf4';
+    const response = await this.fetch(
+      `https://api.spearmint.xyz/projects/${projectID}/attestationSchema`,
+      //'https://api.spearmint.xyz/projects/3fd819d8-8bd5-4d5b-a3b4-ae4820b58bf4/attestationSchema'
+      {
+        headers: {
+          authorization: `Bearer ${apiKey}`,
+          accept: 'application/json',
+          Authorization: 'spsk_PiosaAbiHXn5I1paVlREGP5WfQZ5IleAzwBkSdtL',
+          'content-type': 'application/json',
+        },
+        method: 'PUT',
+        body: JSON.stringify({
+          tentativeStatus: 'selected',
+          shouldFinalize: false,
+        }),
+      },
+    );
+    const data = await handleFetchResponse<GetEntryResponseAPI>(response);
+    return data;
+  }
 
-    async getProof(projectID: string, address: string) {
+  async getProof(projectID: string, apiKey: string, address: string) {
+    const response = await this.fetch(
+      `https://api.spearmint.xyz/projects/${projectID}/proofs/${address}`,
+      {
+        headers: {authorization: `Bearer ${apiKey}`},
 
-    }
+        method: 'get',
+      },
+    );
+    const data = await handleFetchResponse<GetProofAPIResponse>(response);
+    return data;
+  }
 
-    async getEntryStatus(projectID: string, address: string) {
+  async getEntryStatus(projectID: string, apiKey: string, address: string) {
+    const response = await this.fetch(
+      `https://api.spearmint.xyz/projects/${projectID}/entries/${address}/status`,
+      {
+        headers: {authorization: `Bearer ${apiKey}`},
+        method: 'get',
+      },
+    );
+    const data = await handleFetchResponse<GetEntryStatusAPI>(response);
+    return data;
+  }
 
-    }
-
-    async getEntry(projectID: string, address: string) {
-
-    }
+  async getEntry(projectID: string, apiKey: string, address: string) {
+    const response = await this.fetch(
+      `https://api.spearmint.xyz/projects/${projectID}/entries/${address}`,
+      {
+        headers: {authorization: `Bearer ${apiKey}`},
+        method: 'get',
+      },
+    );
+    const data = await handleFetchResponse<GetEntryAPI>(response);
+    return data;
+  }
 }
 
-async function main() {
-
-}
+async function main() {}
